d82db522e07a477d768ebeb6bed4f9c5f1efb548
RajaShahzaibNaseer <shahzaib.naseer.work2@gmail.com>
2025-07-03 19:21:37 +0500

    added game ticks for stable fps

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pygame
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
     
     while True:
         screen.fill((0,0,0))
         pygame.display.flip()
+        time.tick(60)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
